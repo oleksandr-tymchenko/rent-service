66b9c6106db1f83f8c3b82216636dcadb1375107
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-10-11 16:57:05 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/car-rent-service">
+      <BrowserRouter basename="/rent-service">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
